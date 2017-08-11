b11970bf78f8e047e6743fbf1676a2d158a20483
Tom Kent <email@tomkent.me>
2017-08-12 00:30:17 +0100

    Added server variable parser

---
+++ ColonyAPI/ColonyAPI/Helpers/ServerVariableParser.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Pipliz.JSON;
+
+namespace ColonyAPI.Helpers
+{
+    public class ServerVariableParser
+    {
+        private static JSONNode node = new Pipliz.JSON.JSONNode(Pipliz.JSON.NodeType.Object);
+
+        public static void init()
+        {
+            if (Pipliz.JSON.JSON.Deserialize("gamedata/settings/server.json", out node, false))
+            {
+                Utilities.WriteLog("ColonyAPI", "Loaded server variables");
+            }
+        }
+
+        public static string GetVariableAsString(string key)
+        {
+            string[] keys = key.Split('.');
+
+            if (keys.Length > 0)
+            {
+                if (node.HasChild(keys[0]))
+                {
+                    // has child
+                    JSONNode c = new JSONNode(NodeType.Object);
+                    c = node.GetAs<JSONNode>(keys[0]);
+                    return c.GetAs<string>(keys[1]);
+
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            else
+            {
+                return node.GetAs<string>(key);
+            }
+        }
+
+        public static int GetVariableAsInt(string key)
+        {
+            string[] keys = key.Split('.');
+
+
+            if (keys.Length > 0)
+            {
+                if (node.HasChild(keys[0]))
+                {
+                    // has child
+                    JSONNode c = new JSONNode(NodeType.Object);
+                    c = node.GetAs<JSONNode>(keys[0]);
+                    return c.GetAs<int>(keys[1]);
+
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return node.GetAs<int>(key);
+            }
+        }
+
+        public static float GetVariableAsFloat(string key)
+        {
+            string[] keys = key.Split('.');
+
+
+            if (keys.Length > 0)
+            {
+                if (node.HasChild(keys[0]))
+                {
+                    // has child
+                    JSONNode c = new JSONNode(NodeType.Object);
+                    c = node.GetAs<JSONNode>(keys[0]);
+                    return c.GetAs<float>(keys[1]);
+
+                }
+                else
+                {
+                    return 0F;
+                }
+            }
+            else
+            {
+                return node.GetAs<float>(key);
+            }
+        }
+    }
+}
